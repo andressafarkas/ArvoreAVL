34464f095a29b51e23ccd1fb605537d3978277f7
Gabriel Severo <gsevero16@gmail.com>
2022-11-28 18:55:53 -0300

    ultimos testes

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         ArvoreAvl arvore = new ArvoreAvl();
+        ArvoreAvl arvore1 = new ArvoreAvl();
 
         arvore.inserir(1);
         arvore.inserir(2);
@@ -15,11 +16,10 @@ public class Main {
         arvore.inserir(7);
         arvore.inserir(8);
         arvore.inserir(9);
-
+      
         System.out.println("Altura: " + arvore.getAltura());
         arvore.GeraDOT();
         arvore.clear();
-
 
         arvore.inserir(9);
         arvore.inserir(8);
@@ -41,5 +41,46 @@ public class Main {
         System.out.println("contains 5? : " + clone.contains(5));
         System.out.println("contains 12? : " + clone.contains(12));
 
+        
+        arvore.inserir(1);
+        arvore.inserir(2);
+        arvore.inserir(3);
+        arvore.inserir(4);
+        arvore.inserir(5);
+        arvore.inserir(6);
+        arvore.inserir(7);
+        arvore.inserir(8);
+        arvore.inserir(9);
+
+        arvore1.inserir(1);
+        arvore1.inserir(2);
+        arvore1.inserir(3);
+        arvore1.inserir(4);
+        arvore1.inserir(5);
+        arvore1.inserir(6);
+        arvore1.inserir(7);
+        arvore1.inserir(8);
+        arvore1.inserir(9);
+
+        // Verifica se a arvore1 é espelho de arvore (Teste 3)
+        System.out.println(arvore.isMirror(arvore1));
+        arvore.GeraDOT();
+        arvore1.GeraDOT();
+        arvore1.clear();
+
+        arvore1.inserir(9);
+        arvore1.inserir(8);
+        arvore1.inserir(7);
+        arvore1.inserir(6);
+        arvore1.inserir(5);
+        arvore1.inserir(4);
+        arvore1.inserir(3);
+        arvore1.inserir(2);
+        arvore1.inserir(1);
+
+        // Verifica se a arvore1 é espelho de arvore (Teste 3)
+        System.out.println(arvore.isMirror(arvore1));
+        arvore.GeraDOT();
+        arvore1.GeraDOT();
     }
 }
